6ea5991d4df4b5b922b1d8c494cc36f44afd060d
travisbrigman <travis1000@gmail.com>
2021-02-12 11:32:28 -0600

    Initial Commit

---
+++ BookwormCD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookwormCD
+//
+//  Created by Travis Brigman on 2/12/21.
+//  Copyright © 2021 Travis Brigman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
