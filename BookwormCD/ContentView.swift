394b3bc022c012f79898342641f691cc584dddbb
travisbrigman <travis1000@gmail.com>
2021-02-12 21:42:06 -0600

    feat: added delete function to ContentView.swift

--- BookwormCD/ContentView.swift
+++ BookwormCD/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Book.title, ascending: true)]) var books: FetchedResults<Book>
     
     @State private var showingAddscreen = false
     
@@ -31,20 +31,27 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
-                .navigationBarTitle("Bookworm 🐛")
-                .navigationBarItems(trailing: Button(action: { self.showingAddscreen.toggle()
-                    
-                }) {
-                    Image(systemName: "plus")
-                    }
+            .navigationBarTitle("Bookworm 🐛")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: { self.showingAddscreen.toggle()
+                
+            }) {
+                Image(systemName: "plus")
+                }
             )
                 .sheet(isPresented: $showingAddscreen) {
                     AddBookView().environment(\.managedObjectContext, self.moc)
             }
         }
     }
-    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        try? moc.save()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
